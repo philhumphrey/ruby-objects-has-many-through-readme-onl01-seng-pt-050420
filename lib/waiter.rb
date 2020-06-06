c8254c8c5077cc47df16b93ab468721c9767e97b
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-06 17:44:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,8 +20,8 @@ attr_accessor :name, :yrs_experience
  end
  
 def best_tipper
-  best_tipped_meal = meals.max {|meal_a,meal_b| 
-  meal_a.tip <=> meal_b.tip}
+  best_tipped_meal = meals.max do |meal_a, meal_b| 
+  meal_a.tip <=> meal_b.tip
 end
 best_tipped_meal.customer
 end
