7c67b9f0da6ed3e5083af792c16fc399d84dccd7
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-06 17:43:38 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,10 +19,10 @@ attr_accessor :name, :yrs_experience
    Meal.all.select {|meal| meal.waiter == self}
  end
  
- def best_tipper
-   best_tipped_meal = meals.max {|meal_a,meal_b| 
-   meal_a.tip <=> meal_b.tip}
- end
+def best_tipper
+  best_tipped_meal = meals.max {|meal_a,meal_b| 
+  meal_a.tip <=> meal_b.tip}
+end
 best_tipped_meal.customer
 end
 
